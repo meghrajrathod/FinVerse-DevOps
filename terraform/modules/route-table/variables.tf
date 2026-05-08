e879521aea4704ebda15a98af52fe3c819dd4851
meghrajrathod <25meghrajrathod@gmail.com>
2026-05-08 06:06:59 +0530

    Added automated production deployment pipeline

---
+++ terraform/modules/route-table/variables.tf
@@ -0,0 +1,15 @@
+variable "vpc_id" {
+  type = string
+}
+
+variable "igw_id" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "route_table_name" {
+  type = string
+}
