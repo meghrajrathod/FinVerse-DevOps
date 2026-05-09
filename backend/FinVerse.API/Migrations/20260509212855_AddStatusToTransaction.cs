0389fe6171d911dc01ec57677e67684cda2fd4eb
meghrajrathod <25meghrajrathod@gmail.com>
2026-05-10 02:59:11 +0530

    added status migration

---
+++ backend/FinVerse.API/Migrations/20260509212855_AddStatusToTransaction.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinVerse.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatusToTransaction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "Transactions",
+                type: "numeric(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Transactions",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Email",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Transactions");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "Transactions",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(18,2)");
+        }
+    }
+}
