a9abc1c33556d19734c4d23d0391281a92963d7a
meghrajrathod <25meghrajrathod@gmail.com>
2026-05-12 18:29:02 +0530

    change api.ts file

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://api.meghrajrathod.cloud/api",
+  baseURL: "https://api.meghrajrathod.cloud/api",
 });
 
 export default api;
