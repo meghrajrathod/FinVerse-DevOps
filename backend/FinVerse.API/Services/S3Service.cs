cb4922926bb675d1678f203f9440e67c75ae86a0
meghrajrathod <25meghrajrathod@gmail.com>
2026-05-13 06:54:27 +0530

    fixed s3 region endpoint

--- backend/FinVerse.API/Services/S3Service.cs
+++ backend/FinVerse.API/Services/S3Service.cs
@@ -16,7 +16,11 @@ namespace FinVerse.API.Services
             var accessKey = Environment.GetEnvironmentVariable("AWS_ACCESS_KEY_ID");
             var secretKey = Environment.GetEnvironmentVariable("AWS_SECRET_ACCESS_KEY");
 
-            var region = RegionEndpoint.USEast1;
+                var config = new AmazonS3Config
+    {
+        RegionEndpoint = RegionEndpoint.USEast1,
+        ForcePathStyle = true
+    };
 
             _s3Client = new AmazonS3Client(accessKey, secretKey, region);
         }
